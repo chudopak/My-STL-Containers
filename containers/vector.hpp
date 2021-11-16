fd4f58527bc2971603cf2f30faa380799053d045
Stepan Kirillov <stepakirillov59@gmail.com>
2021-11-16 20:28:04 +0300

    Fresh start

---
+++ containers/vector.hpp
@@ -0,0 +1,24 @@
+#ifndef VECTOR_HPP
+# define VECTORL_HPP
+
+# include <memory>
+# include <algorithm>
+# include <cstddef>
+# include <tgmath.h>
+
+namespace ft
+{
+	template < class T, class Alloc = std::allocator<T> >
+	class vector {
+
+	public:
+		typedef T											value_type;
+		typedef Alloc										allocator_type;
+		typedef typename allocator_type::reference			reference;
+		typedef typename allocator_type::const_reference	const_reference;
+		typedef typename allocator_type::pointer			pointer;
+		typedef typename allocator_type::const_pointer		const_pointer;
+	};
+}
+
+#endif
